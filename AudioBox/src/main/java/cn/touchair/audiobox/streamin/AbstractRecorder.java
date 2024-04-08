d434c73e4430dd9aa16cafa5902ab93140635d33
De.Liu <de.liu@touchair.cn>
2024-04-08 13:17:53 +0800

    Feature: 一处优化

--- AudioBox/src/main/java/cn/touchair/audiobox/streamin/AbstractRecorder.java
+++ AudioBox/src/main/java/cn/touchair/audiobox/streamin/AbstractRecorder.java
@@ -3,10 +3,6 @@ package cn.touchair.audiobox.streamin;
 import android.media.AudioFormat;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
 
 import cn.touchair.audiobox.common.PrettyTextUtils;
 import cn.touchair.audiobox.interfaces.AudioComponents;
@@ -14,13 +10,12 @@ import cn.touchair.audiobox.interfaces.CaptureListener;
 
 public abstract class AbstractRecorder<T> extends AudioComponents {
     protected final String TAG = getClass().getSimpleName();
-
     protected static final int DEFAULT_CHANNEL_MASK = AudioFormat.CHANNEL_IN_STEREO;
     protected static final int DEFAULT_ENCODING = AudioFormat.ENCODING_PCM_16BIT;
     protected static final int DEFAULT_SAMPLE_RATE = 48000;
-    protected static final byte BUFFER_TYPE_SHORT = 1;
-    protected static final byte BUFFER_TYPE_BYTE = 1 << 1;
-    protected static final byte BUFFER_TYPE_FLOAT = 1 << 2;
+    public static final byte BUFFER_TYPE_SHORT = 1;
+    public static final byte BUFFER_TYPE_BYTE = 1 << 1;
+    public static final byte BUFFER_TYPE_FLOAT = 1 << 2;
     protected AudioFormat format;
     protected byte bufferType;
     protected CaptureListener<T> listener;
@@ -29,27 +24,19 @@ public abstract class AbstractRecorder<T> extends AudioComponents {
         showParameters();
     }
 
-    public void setCaptureListener(@NonNull CaptureListener<T> listener) {
-        checkAndSetBufferType(listener);
+    public void setCaptureListener(@NonNull CaptureListener<T> listener, byte captureType) {
+        bufferType = checkBufferType(captureType);
         this.listener = listener;
     }
 
-    private void checkAndSetBufferType(@NonNull CaptureListener<T> listener) {
-        Type[] genericInterfaces = listener.getClass().getGenericInterfaces();
-        if (genericInterfaces.length == 0) {
-            throw new RuntimeException("Unable set buffer type.");
-        }
-        ParameterizedType parameterizedType = (ParameterizedType) genericInterfaces[0];
-        Type type = parameterizedType.getActualTypeArguments()[0];
-        final String arrayTypeName = type.toString();
-        if (arrayTypeName.equals(byte[].class.getCanonicalName())) {
-            this.bufferType = BUFFER_TYPE_BYTE;
-        } else if (arrayTypeName.equals(short[].class.getCanonicalName())) {
-            this.bufferType = BUFFER_TYPE_SHORT;
-        } else if (arrayTypeName.equals(float[].class.getCanonicalName())) {
-            this.bufferType = BUFFER_TYPE_FLOAT;
-        } else {
-            throw new RuntimeException("Type " + arrayTypeName + " not support!");
+    private byte checkBufferType(byte type) {
+        switch (type) {
+            case BUFFER_TYPE_BYTE:
+            case BUFFER_TYPE_FLOAT:
+            case BUFFER_TYPE_SHORT:
+                return type;
+            default:
+                throw new RuntimeException("Type " + type + " not support!");
         }
     }
 
