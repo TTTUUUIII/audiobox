1c33b9dc2d50f6429655b88cb18bb0f581614fc8
De.Liu <de.liu@touchair.cn>
2024-06-25 09:19:08 +0800

    Feature: 降低JDK版本至1.8

--- AudioBox/build.gradle.kts
+++ AudioBox/build.gradle.kts
@@ -32,14 +32,14 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
 }
 
 dependencies {
 
-    implementation("androidx.appcompat:appcompat:1.6.1")
+    compileOnly("androidx.appcompat:appcompat:1.6.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
@@ -51,7 +51,7 @@ afterEvaluate {
             register("defaultAar", MavenPublication::class.java) {
                 from(components["release"])
                 groupId = "com.github.TTTUUUIII"
-                version = "1.1.0"
+                version = "1.3.1"
             }
         }
     }
